e3ab76e0d75641bdcbd0bc5bc1ddf4ef164a6136
Simon Cropp <simon.cropp@gmail.com>
2019-05-28 12:41:26 +1000

    re verify

--- src/Tests/SeqWriterTests.cs
+++ src/Tests/SeqWriterTests.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Threading;
 using System.Threading.Tasks;
 using ObjectApproval;
 using SeqProxy;
@@ -14,7 +13,7 @@ public class SeqWriterTests :
     {
         var httpFactory = new MockClientFactory();
         var writer = new SeqWriter(httpFactory, "http://theSeqUrl", "theAppName", new Version(1, 2), "theApiKey", false);
-        var mockRequest = new MockRequest("{'@mt':'Simple Message'}");
+        var mockRequest = new MockRequest("{'@t':'2019-05-28T12:40:41.3744709+10:00','@mt':'Simple Message'}");
         await writer.Handle(ClaimsBuilder.Build(), mockRequest, new MockResponse());
         ObjectApprover.VerifyWithJson(httpFactory.Client.Requests, s => s.Replace(Environment.MachineName, "TheMachineName"));
     }

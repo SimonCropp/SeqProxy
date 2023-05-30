a2a6ac7c048320b137c625fe00262d2850d43d86
Simon <simon.cropp@gmail.com>
2023-05-31 09:21:34 +1000

    cleanup

---
+++ src/SeqProxy/GlobalUsings.cs
@@ -0,0 +1,10 @@
+﻿// Global using directives
+
+global using System.Security.Claims;
+global using System.Web;
+global using Microsoft.AspNetCore.Authentication;
+global using Microsoft.AspNetCore.Authorization;
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.Net.Http.Headers;
+global using SeqProxy;
